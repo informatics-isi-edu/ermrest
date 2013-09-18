3bfd9c6aff8740a570a721b1d7870647bb4896b4
schuler <robes@users.noreply.github.com>
2013-09-18 22:56:51 +0000

    Added introspection of pkeys and fkeys.

--- src/model.py
+++ src/model.py
@@ -51,6 +51,64 @@ WHERE c.table_schema NOT IN ('information_schema', 'pg_catalog')
 ORDER BY c.table_catalog, c.table_schema, c.table_name, c.ordinal_position;
     '''
     
+    # Select the unique or primary key columns
+    PKEY_COLUMNS = '''
+SELECT
+   k_c_u.constraint_schema,
+   k_c_u.constraint_name,
+   k_c_u.table_schema,
+   k_c_u.table_name,
+   k_c_u.column_name
+FROM information_schema.key_column_usage k_c_u
+JOIN information_schema.table_constraints t_c
+ON k_c_u.constraint_schema =
+   t_c.constraint_schema
+   AND
+   k_c_u.constraint_name =
+   t_c.constraint_name 
+WHERE t_c.constraint_type IN ('UNIQUE', 'PRIMARY KEY')
+ORDER BY
+   k_c_u.constraint_name,
+   k_c_u.ordinal_position
+;
+    '''
+
+    # Select the foreign key reference columns
+    #
+    # The following query was adapted from an example here:
+    # http://msdn.microsoft.com/en-us/library/aa175805%28SQL.80%29.aspx
+    FKEY_COLUMNS = '''
+SELECT
+     KCU1.CONSTRAINT_SCHEMA AS FK_CONSTRAINT_SCHEMA
+   , KCU1.CONSTRAINT_NAME AS FK_CONSTRAINT_NAME
+   , KCU1.TABLE_SCHEMA AS FK_TABLE_SCHEMA
+   , KCU1.TABLE_NAME AS FK_TABLE_NAME
+   , KCU1.COLUMN_NAME AS FK_COLUMN_NAME
+   , KCU1.ORDINAL_POSITION AS FK_ORDINAL_POSITION
+   , KCU2.TABLE_SCHEMA AS UQ_TABLE_SCHEMA
+   , KCU2.TABLE_NAME AS UQ_TABLE_NAME
+   , KCU2.COLUMN_NAME AS UQ_COLUMN_NAME
+   , KCU2.ORDINAL_POSITION AS UQ_ORDINAL_POSITION
+   , RC.DELETE_RULE AS RC_DELETE_RULE
+   , RC.UPDATE_RULE AS RC_UPDATE_RULE
+FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS RC
+JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE KCU1
+ON KCU1.CONSTRAINT_CATALOG = RC.CONSTRAINT_CATALOG
+   AND KCU1.CONSTRAINT_SCHEMA = RC.CONSTRAINT_SCHEMA
+   AND KCU1.CONSTRAINT_NAME = RC.CONSTRAINT_NAME
+JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE KCU2
+ON KCU2.CONSTRAINT_CATALOG =
+RC.UNIQUE_CONSTRAINT_CATALOG
+   AND KCU2.CONSTRAINT_SCHEMA =
+RC.UNIQUE_CONSTRAINT_SCHEMA
+   AND KCU2.CONSTRAINT_NAME =
+RC.UNIQUE_CONSTRAINT_NAME
+   AND KCU2.ORDINAL_POSITION = KCU1.ORDINAL_POSITION
+ORDER BY
+   KCU1.CONSTRAINT_NAME, KCU1.ORDINAL_POSITION
+;
+    '''
+    
     # PostgreSQL denotes array types with the string 'ARRAY'
     ARRAY_TYPE = 'ARRAY'
     
@@ -58,12 +116,16 @@ ORDER BY c.table_catalog, c.table_schema, c.table_name, c.ordinal_position;
     schemas  = dict()
     tables   = dict()
     columns  = dict()
-    
-    cur = conn.execute(SELECT_COLUMNS)
-    tuples = cur.fetchall()
+    pkeys    = dict()
+    fkeys    = dict()
 
     model = Model()
-
+    
+    #
+    # Introspect schemas, tables, columns
+    #
+    cur = conn.execute(SELECT_COLUMNS)
+    tuples = cur.fetchall()
     for tup in tuples:
         # Qualified name from tuple
         dname = tup[0]
@@ -96,7 +158,78 @@ ORDER BY c.table_catalog, c.table_schema, c.table_name, c.ordinal_position;
         columns.setdefault((dname, sname, tname, cname), 
             Column(tables[(dname, sname, tname)], cname, position, base_type, 
                    is_array, default_value))
+
+    #
+    # Introspect uniques / primary key references
+    #
+    cur = conn.execute(PKEY_COLUMNS)
+    tuples = cur.fetchall()
+    for tup in tuples:
+        pk_constraint_key   = (tup[0], tup[1]),
+        pk_table_schema     = tup[2]
+        pk_table_name       = tup[3]
+        pk_column_name      = tup[4]
         
+        # Get recorded objects
+        pk_table = tables[(dname, pk_table_schema, pk_table_name)]
+        pk_col = columns[(dname, pk_table_schema, pk_table_name, pk_column_name)]
+        
+        # Get or create pkey object
+        if pk_constraint_key not in pkeys:
+            pkeys[pk_constraint_key] = Unique(pk_table)
+        pkey = pkeys[pk_constraint_key]
+        
+        # Add pk column
+        pkey.columns[pk_column_name] = pk_col
+    
+    # Link tables to primary keys
+    for pkey in pkeys.values():
+        pkey.table.uniques[tuple(pkey.columns.values())] = pkey
+
+    #
+    # Introspect foreign key references
+    #
+    cur = conn.execute(FKEY_COLUMNS)
+    tuples = cur.fetchall()
+    for tup in tuples:
+        fk_constraint_key   = (tup[0], tup[1])
+        fk_table_schema     = tup[2]
+        fk_table_name       = tup[3]
+        fk_column_name      = tup[4]
+        fk_column_pos       = tup[5]
+        uq_table_schema     = tup[6]
+        uq_table_name       = tup[7]
+        uq_column_name      = tup[8]
+        uq_column_pos       = tup[9]
+        on_delete           = tup[10]
+        on_update           = tup[11]
+        
+        # Get recorded objects
+        fk_table = tables[(dname, fk_table_schema, fk_table_name)]
+        fk_col = columns[(dname, fk_table_schema, fk_table_name, fk_column_name)]
+        uq_table = tables[(dname, uq_table_schema, uq_table_name)]
+        uq_col = columns[(dname, uq_table_schema, uq_table_name, uq_column_name)]
+        
+        # Get or create fkey object
+        if fk_constraint_key not in fkeys:
+            fkeys[fk_constraint_key] = ForeignKey(fk_table)
+        fkey = fkeys[fk_constraint_key]
+        
+        # Add fk column
+        fkey.columns[fk_column_pos] = fk_col
+        
+        # Get or create key reference
+        if uq_table not in fkey.keyreferences:
+            fkey.keyreferences[uq_table] = KeyReference(fkey, uq_table, on_delete, on_update)
+        keyref = fkey.keyreferences[uq_table]
+        
+        # Add key ref column
+        keyref.columns[uq_column_pos] = uq_col
+    
+    # Link tables to foreign keys
+    for fkey in fkeys.values():
+        fkey.table.fkeys[tuple(fkey.columns.values())] = fkey
+    
     return model
 
 def __pg_default_value(base_type, raw):
@@ -167,6 +300,8 @@ class Table:
         self.schema = schema
         self.name = name
         self.columns = dict()
+        self.uniques = dict()
+        self.fkeys = dict()
         
         if name not in self.schema.tables:
             self.schema.tables[name] = self
@@ -174,7 +309,15 @@ class Table:
     def verbose(self):
         s = "name: %s, num_columns: %d\n" % (self.name, len(self.columns))
         for col in self.columns.values():
-            s += "Column: " + str(col) + "\n"
+            s += "Column: " + col.verbose() + "\n"
+        s += "--------------------------------------------------------------\n"
+        s += "num_uniques: %d\n" % len(self.uniques)
+        for uq in self.uniques.values():
+            s += uq.verbose() + "\n"
+        s += "--------------------------------------------------------------\n"
+        s += "num_fkeys: %d\n" % len(self.fkeys)
+        for fkey in self.fkeys.values():
+            s += fkey.verbose()
         s += "--------------------------------------------------------------\n"
         return s
 
@@ -203,19 +346,62 @@ class Column:
         if name not in self.table.columns:
             self.table.columns[name] = self
     
-    def __str__(self):
+    def verbose(self):
         return "name: %s, position: %d, base_type: %s, is_array: %s, default_value: %s" \
                 % (self.name, self.position, self.base_type, self.is_array, self.default_value)
 
 class Unique:
     """A unique constraint."""
-    pass
+    
+    def __init__(self, table):
+        self.table = table
+        self.columns = dict()
+        
+    def verbose(self):
+        s = '('
+        for col in self.columns.values():
+            s += col.name + ','
+        s += ')'
+        return s
 
 class ForeignKey:
     """A foreign key."""
-    pass
+
+    def __init__(self, table):
+        self.table = table
+        self.columns = dict()
+        self.keyreferences = dict()
+        # We don't link into table's fkey dict yet, because we the
+        # fkey dict is to be keyed on a tuple of the columns in the fkey
+        
+    def verbose(self):
+        s = "FKEY COLS:\n"
+        for col in self.columns:
+            s += "Position: %s -- %s\n" % (col, self.columns[col].verbose())
+        
+        for kref in self.keyreferences.values():
+            s += "REFERENCES: table name: %s\n" % kref.table.name
+            for col in kref.columns:
+                s += "Position: %s -- %s\n" % (col, kref.columns[col].verbose())
+        return s
 
 class KeyReference:
     """A reference from a foreign key to a primary key."""
-    pass
+    
+    def __init__(self, foreign_key, table, on_delete, on_update):
+        self.foreign_key = foreign_key
+        self.table = table
+        self.on_delete = on_delete
+        self.on_update = on_update
+        self.columns = dict()
+        # Link into foreign key's key reference list, by table ref
+        self.foreign_key.keyreferences[table] = self
 
+
+if __name__ == '__main__':
+    import os, sanepg2
+    connstr = "dbname=%s user=%s" % \
+        (os.getenv('TEST_DBNAME', 'test'), os.getenv('TEST_USER', 'test'))
+    m = introspect(sanepg2.connection(connstr))
+    print m.verbose()
+    exit(0)
