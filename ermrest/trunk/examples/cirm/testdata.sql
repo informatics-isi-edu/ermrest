2f4d2850007580e3ce8af7870975f4c94995f901
schuler <schuler@22429d68-4e6d-4b4f-8b83-96928a890c38>
2013-11-21 01:21:22 +0000

    Added box entity. Updated scan and slide. PKs are now fixed length character fields that must be supplied by the caller. Test data updated for the new model.

--- ermrest/trunk/examples/cirm/testdata.sql
+++ ermrest/trunk/examples/cirm/testdata.sql
@@ -1,19 +1,22 @@
 
-COPY cirm.slide (id, experiment_date, sequence_num, genotype, comment, initials, revision) from stdin with csv delimiter ',' quote '"';
-1,2013-11-01,1,wnt1creZEGG,"antibody used",RES,00
-2,2013-11-02,83,wnt1creZEGG,"antibody used",RES,00
-3,2013-11-03,21,wnt1creZEGG,"antibody used",RES,00
-4,2013-11-04,9,wnt1creZEGG,"antibody used",RES,00
-5,2013-11-05,59,wnt1creZEGG,"antibody used",RES,00
-6,2013-11-06,3,wnt1creZEGG,"antibody used",RES,00
+COPY cirm.box (id, section_date, sample_name, initials, disambiguator, comment) from stdin with csv delimiter ',' quote '"';
+20131108-wnt1creZEGG-RES-0,2013-11-08,wnt1creZEGG,RES,0,"This is a box of origin"
 \.
 
-COPY cirm.scan (id, slide_id, filename,thumbnail,tiles,comment) from stdin with csv delimiter ',' quote '"';
-1,1,"1.czi","1.thumb.jpeg","1/","good"
-2,2,"2.czi","2.thumnb.jpeg","2/","good"
-3,4,"3.czi","3.thumnb.jpeg","3/","good"
-4,6,"4.czi","4.thumnb.jpeg","4/","good"
-5,1,"5.czi","5.thumnb.jpeg","5/","good"
+COPY cirm.slide (id, box_of_origin_id, sequence_num, revision, experiment_date, experiment_description, initials, comment) from stdin with csv delimiter ',' quote '"';
+20131108-wnt1creZEGG-RES-0-38-000,20131108-wnt1creZEGG-RES-0,38,0,2013-11-20,"antibody used",RES,"This is a slide"
+20131108-wnt1creZEGG-RES-0-38-001,20131108-wnt1creZEGG-RES-0,38,1,2013-11-21,"antibody used",RES,"This is a slide"
+20131108-wnt1creZEGG-RES-0-12-000,20131108-wnt1creZEGG-RES-0,12,0,2013-11-20,"antibody used",RES,"This is a slide"
+20131108-wnt1creZEGG-RES-0-84-000,20131108-wnt1creZEGG-RES-0,84,0,2013-11-19,"antibody used",RES,"This is a slide"
+20131108-wnt1creZEGG-RES-0-55-000,20131108-wnt1creZEGG-RES-0,55,0,2013-11-22,"antibody used",RES,"This is a slide"
+20131108-wnt1creZEGG-RES-0-09-000,20131108-wnt1creZEGG-RES-0,9,0,2013-11-22,"antibody used",RES,"This is a slide"
 \.
 
+COPY cirm.scan (id, slide_id, scan_num, filename, thumbnail, tilesdir, comment) from stdin with csv delimiter ',' quote '"';
+20131108-wnt1creZEGG-RES-0-38-000-000,20131108-wnt1creZEGG-RES-0-38-000,0,20131108-wnt1creZEGG-RES-0-38-000.czi,20131108-wnt1creZEGG-RES-0-38-000.jpeg,20131108-wnt1creZEGG-RES-0-38-000/,"This is a scan"
+20131108-wnt1creZEGG-RES-0-38-000-001,20131108-wnt1creZEGG-RES-0-38-000,1,20131108-wnt1creZEGG-RES-0-38-000(2).czi,20131108-wnt1creZEGG-RES-0-38-000(2).jpeg,20131108-wnt1creZEGG-RES-0-38-000(2)/,"This is a scan"
+20131108-wnt1creZEGG-RES-0-38-001-000,20131108-wnt1creZEGG-RES-0-38-001,0,20131108-wnt1creZEGG-RES-0-38-001.czi,20131108-wnt1creZEGG-RES-0-38-001.jpeg,20131108-wnt1creZEGG-RES-0-38-001/,"This is a scan"
+20131108-wnt1creZEGG-RES-0-12-000-000,20131108-wnt1creZEGG-RES-0-12-000,0,20131108-wnt1creZEGG-RES-0-12-000.czi,20131108-wnt1creZEGG-RES-0-12-000.jpeg,20131108-wnt1creZEGG-RES-0-12-000/,"This is a scan"
+20131108-wnt1creZEGG-RES-0-84-000-000,20131108-wnt1creZEGG-RES-0-84-000,0,20131108-wnt1creZEGG-RES-0-84-000.czi,20131108-wnt1creZEGG-RES-0-84-000.jpeg,20131108-wnt1creZEGG-RES-0-84-000/,"This is a scan"
+\.
 
