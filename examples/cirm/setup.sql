2f8290e68fdd67b24f68f098d1d36c47f7d8f0f4
schuler <robes@users.noreply.github.com>
2013-11-21 01:21:22 +0000

    Added box entity. Updated scan and slide. PKs are now fixed length character fields that must be supplied by the caller. Test data updated for the new model.

--- examples/cirm/setup.sql
+++ examples/cirm/setup.sql
@@ -1,31 +1,47 @@
 CREATE SCHEMA cirm;
 
-CREATE TABLE cirm.slide
+CREATE TABLE cirm.box
   (
-    id bigserial PRIMARY KEY,
-    experiment_date date,
-    sequence_num integer,
-    genotype text,
-    comment text,
-    initials text,
-    revision integer
+    id char(30) PRIMARY KEY,
+    section_date date NOT NULL,
+    sample_name char(15) NOT NULL,
+    initials char(3) NOT NULL,
+    disambiguator char(1) NOT NULL,
+    comment text
   );
 
-CREATE INDEX ON cirm.slide USING gin ( (to_tsvector('english', genotype)) );
-CREATE INDEX ON cirm.slide USING gin ( (to_tsvector('english', comment)) );
+CREATE INDEX ON cirm.box USING gin ( (to_tsvector('english', sample_name)) );
+CREATE INDEX ON cirm.box USING gin ( (to_tsvector('english', initials)) );
+CREATE INDEX ON cirm.box USING gin ( (to_tsvector('english', comment)) );
+
+CREATE TABLE cirm.slide
+  (
+    id char(37) PRIMARY KEY,
+    box_of_origin_id char(30) NOT NULL,
+    sequence_num integer NOT NULL,
+    revision integer NOT NULL,
+    experiment_date date NOT NULL,
+    experiment_description char(15) NOT NULL,
+    initials char(3) NOT NULL,
+    comment text,
+    FOREIGN KEY (box_of_origin_id) REFERENCES cirm.box (id)
+  );
+
+CREATE INDEX ON cirm.slide USING gin ( (to_tsvector('english', experiment_description)) );
 CREATE INDEX ON cirm.slide USING gin ( (to_tsvector('english', initials)) );
+CREATE INDEX ON cirm.slide USING gin ( (to_tsvector('english', comment)) );
 
 CREATE TABLE cirm.scan
   (
-    id bigserial PRIMARY KEY,
-    slide_id bigint,
+    id char(41) PRIMARY KEY,
+    slide_id char(37),
+    scan_num integer NOT NULL,
     filename text,
     thumbnail text,
-    tiles text,
+    tilesdir text,
     comment text,
     FOREIGN KEY (slide_id) REFERENCES cirm.slide (id)
   );
 
--- Note: english config doesn't seem to parse URIs very well...
 CREATE INDEX ON cirm.scan USING gin ( (to_tsvector('english', comment)) );
 
