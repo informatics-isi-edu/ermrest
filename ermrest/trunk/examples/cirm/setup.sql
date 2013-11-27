c6f35e900b35d2afb222692465d0de11550d1cb1
schuler <schuler@22429d68-4e6d-4b4f-8b83-96928a890c38>
2013-11-27 05:41:39 +0000

    Updated cirm schema:  -- added the 'experiment' table  -- added single FK from 'slide' to 'experiment' table  -- removed unwanted columns from 'slide' table  -- updated indexes accordingly

--- ermrest/trunk/examples/cirm/setup.sql
+++ ermrest/trunk/examples/cirm/setup.sql
@@ -14,21 +14,32 @@ CREATE INDEX ON cirm.box USING gin ( (to_tsvector('english', sample_name)) );
 CREATE INDEX ON cirm.box USING gin ( (to_tsvector('english', initials)) );
 CREATE INDEX ON cirm.box USING gin ( (to_tsvector('english', comment)) );
 
-CREATE TABLE cirm.slide
+CREATE TABLE cirm.experiment
   (
-    id varchar(37) PRIMARY KEY,
-    box_of_origin_id varchar(30) NOT NULL,
-    sequence_num integer NOT NULL,
-    revision integer NOT NULL,
+    id varchar(30) PRIMARY KEY,
     experiment_date date NOT NULL,
     experiment_description varchar(15) NOT NULL,
     initials varchar(3) NOT NULL,
-    comment text,
-    FOREIGN KEY (box_of_origin_id) REFERENCES cirm.box (id)
+    disambiguator char(1) NOT NULL,
+    comment text
   );
 
-CREATE INDEX ON cirm.slide USING gin ( (to_tsvector('english', experiment_description)) );
-CREATE INDEX ON cirm.slide USING gin ( (to_tsvector('english', initials)) );
+CREATE INDEX ON cirm.experiment USING gin ( (to_tsvector('english', experiment_description)) );
+CREATE INDEX ON cirm.experiment USING gin ( (to_tsvector('english', initials)) );
+CREATE INDEX ON cirm.experiment USING gin ( (to_tsvector('english', comment)) );
+
+CREATE TABLE cirm.slide
+  (
+    id varchar(37) PRIMARY KEY,
+    sequence_num integer NOT NULL,
+    revision integer NOT NULL,
+    box_of_origin_id varchar(30) NOT NULL,
+    experiment_id varchar(30),
+    comment text,
+    FOREIGN KEY (box_of_origin_id) REFERENCES cirm.box (id),
+    FOREIGN KEY (experiment_id) REFERENCES cirm.experiment (id)
+  );
+
 CREATE INDEX ON cirm.slide USING gin ( (to_tsvector('english', comment)) );
 
 CREATE TABLE cirm.scan
