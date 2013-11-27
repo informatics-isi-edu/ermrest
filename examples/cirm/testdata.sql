dc5c8234e02f0084273834cb807d5e3879c56b66
schuler <robes@users.noreply.github.com>
2013-11-27 05:41:39 +0000

    Updated cirm schema:  -- added the 'experiment' table  -- added single FK from 'slide' to 'experiment' table  -- removed unwanted columns from 'slide' table  -- updated indexes accordingly

--- examples/cirm/testdata.sql
+++ examples/cirm/testdata.sql
@@ -3,13 +3,18 @@ COPY cirm.box (id, section_date, sample_name, initials, disambiguator, comment) 
 20131108-wnt1creZEGG-RES-0,2013-11-08,wnt1creZEGG,RES,0,"This is a box of origin"
 \.
 
-COPY cirm.slide (id, box_of_origin_id, sequence_num, revision, experiment_date, experiment_description, initials, comment) from stdin with csv delimiter ',' quote '"';
-20131108-wnt1creZEGG-RES-0-38-000,20131108-wnt1creZEGG-RES-0,38,0,2013-11-20,"antibody used",RES,"This is a slide"
-20131108-wnt1creZEGG-RES-0-38-001,20131108-wnt1creZEGG-RES-0,38,1,2013-11-21,"antibody used",RES,"This is a slide"
-20131108-wnt1creZEGG-RES-0-12-000,20131108-wnt1creZEGG-RES-0,12,0,2013-11-20,"antibody used",RES,"This is a slide"
-20131108-wnt1creZEGG-RES-0-84-000,20131108-wnt1creZEGG-RES-0,84,0,2013-11-19,"antibody used",RES,"This is a slide"
-20131108-wnt1creZEGG-RES-0-55-000,20131108-wnt1creZEGG-RES-0,55,0,2013-11-22,"antibody used",RES,"This is a slide"
-20131108-wnt1creZEGG-RES-0-09-000,20131108-wnt1creZEGG-RES-0,9,0,2013-11-22,"antibody used",RES,"This is a slide"
+COPY cirm.experiment (id, experiment_date, experiment_description, initials, disambiguator, comment) from stdin with csv delimiter ',' quote '"';
+20131112-myantibody1-SV-0,2013-11-12,myantibody1,SV,0,"This is Serban's experiment"
+20131115-myantibody2-KC-0,2013-11-15,myantibody2,KC,0,"This is Karl's experiment"
+\.
+
+COPY cirm.slide (id, box_of_origin_id, experiment_id, sequence_num, revision, comment) from stdin with csv delimiter ',' quote '"';
+20131108-wnt1creZEGG-RES-0-38-000,20131108-wnt1creZEGG-RES-0,,38,0,"This is a slide"
+20131108-wnt1creZEGG-RES-0-38-001,20131108-wnt1creZEGG-RES-0,20131112-myantibody1-SV-0,38,1,"This is a slide"
+20131108-wnt1creZEGG-RES-0-12-000,20131108-wnt1creZEGG-RES-0,20131112-myantibody1-SV-0,12,0,"This is a slide"
+20131108-wnt1creZEGG-RES-0-84-000,20131108-wnt1creZEGG-RES-0,,84,0,"This is a slide"
+20131108-wnt1creZEGG-RES-0-55-000,20131108-wnt1creZEGG-RES-0,20131115-myantibody2-KC-0,55,0,"This is a slide"
+20131108-wnt1creZEGG-RES-0-09-000,20131108-wnt1creZEGG-RES-0,20131115-myantibody2-KC-0,9,0,"This is a slide"
 \.
 
 COPY cirm.scan (id, slide_id, scan_num, filename, thumbnail, tilesdir, comment) from stdin with csv delimiter ',' quote '"';
