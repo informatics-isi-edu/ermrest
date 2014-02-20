a6dcf79a1cd10f41642ffae974776f94e1c8c4f0
karlcz <karlcz@22429d68-4e6d-4b4f-8b83-96928a890c38>
2014-02-20 22:43:25 +0000

    use SQL COALESCE to map NULL text fields to empty string for free-text search feature

--- ermrest/trunk/src/model.py
+++ ermrest/trunk/src/model.py
@@ -474,7 +474,7 @@ class FreetextColumn (Column):
     def sql_name_with_talias(self, talias):
         colnames = [ '%s.%s' % (talias, c.sql_name()) for c in self.srccols ]
         if colnames:
-            return "to_tsvector('english'::regconfig, %s::text)" % (' || '.join(colnames))
+            return "to_tsvector('english'::regconfig, %s)" % (' || '.join([ "COALESCE(%s::text,''::text)" % name for name in colnames ]))
         else:
             return "to_tsvector('english'::regconfig, ''::text)"
 
