fc343310d14628bf6dd214121dc034d5c20c8c72
karlcz <karlcz@users.noreply.github.com>
2014-08-19 21:17:31 +0000

    allow ermrest module to be imported without service config json file

--- src/ermrest_apis.py
+++ src/ermrest_apis.py
@@ -109,7 +109,10 @@ AttrNest = webauthn2_handler_factory.AttrNest
 
 ## setup registry
 registry_config = global_env.get('registry')
-registry = get_registry(registry_config)
+if registry_config:
+    registry = get_registry(registry_config)
+else:
+    registry = None
 
 ## setup catalog factory
 catalog_factory_config = global_env.get('catalog_factory')
