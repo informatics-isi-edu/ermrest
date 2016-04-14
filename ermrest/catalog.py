94d19b5591599ae7e9817bfd89bbc1f522f215e4
Karl Czajkowski <karlcz@isi.edu>
2016-04-13 21:25:49 -0700

    remove trailing comma that is invalid in SQL

--- ermrest/catalog.py
+++ ermrest/catalog.py
@@ -461,7 +461,7 @@ $$ LANGUAGE plpgsql;
 
         cur.execute("""
 GRANT SELECT -- INSERT, UPDATE, DELETE
-  ON _ermrest.meta, _ermrest.model_pseudo_key, _ermrest.model_pseudo_keyref, 
+  ON _ermrest.meta, _ermrest.model_pseudo_key, _ermrest.model_pseudo_keyref
   TO ermrest;
 GRANT SELECT, INSERT, UPDATE, DELETE
   ON _ermrest.model_version, _ermrest.data_version 
