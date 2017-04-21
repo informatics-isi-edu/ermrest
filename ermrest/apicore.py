9ed7fcee8be85dbfd3ec79cfeb551b48cd5eb7de
Karl Czajkowski <karlcz@isi.edu>
2017-04-21 16:19:09 -0700

    add user-tracking info to syslog output
    - requires latest webauthn for Context.tracking
    - only if tracking ID is returned by webauthn
    - only works if mod_usertrack is enabled appropriately on host
    - won't work in practice until mod_webauthn passes tracking cookie to webauthn

--- ermrest/apicore.py
+++ ermrest/apicore.py
@@ -249,6 +249,7 @@ def request_final():
             ('referrer', web.ctx.env.get('HTTP_REFERER')),
             ('agent', web.ctx.env.get('HTTP_USER_AGENT')),
             ('session', web.ctx.webauthn2_context.session),
+            ('track', web.ctx.webauthn2_context.tracking),
         ]
         if v
     ])
