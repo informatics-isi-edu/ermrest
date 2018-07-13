7f20f4d582d0454cce6e10740767088a13c5a53a
Cris Williams <cristina@isi.edu>
2018-07-13 15:42:14 -0700

    Deployed 6e86096 with MkDocs version: 0.17.4

--- ermrest/ermrest_schema.sql
+++
@@ -1,7 +0,0 @@
-
-DROP SCHEMA _ermrest_overlay ;
-
-\set ON_ERROR_STOP
-
-CREATE SCHEMA _ermrest_overlay ;
-
--- test/resttest/__main__.py
+++
@@ -1,6 +0,0 @@
-
-import unittest
-from __init__ import *
-
-
-unittest.main(verbosity=1)
